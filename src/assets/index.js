497d95bd28da8b5b72a5fb8cfa5c40ace1e7f3ae
nuhptr <nugrahaadi733@gmail.com>
2024-04-26 21:51:14 +0700

    src/assets:add assets

---
+++ src/assets/index.js
@@ -0,0 +1,150 @@
+// General Assets
+import icBrainwave from "./ic_brainwave.svg"
+import icCheck from "./ic_check.svg"
+import icBrainwaveSymbol from "./ic_brainwave-symbol.svg"
+import icBrainwaveWhiteSymbol from "./ic_brainwave-symbol-white.svg"
+import icPlay from "./ic_play.svg"
+import icCheck2 from "./ic_check-02.svg"
+import icLoading1 from "./ic_loading-01.svg"
+import icYourlogo from "./ic_yourlogo.svg"
+import icHomeSmile from "./ic_home-smile.svg"
+import icFile02 from "./ic_file-02.svg"
+import icSearchMd from "./ic_search-md.svg"
+import icPlusSquare from "./ic_plus-square.svg"
+import icRecording01 from "./ic_recording-01.svg"
+import icRecording03 from "./ic_recording-03.svg"
+import icDisc02 from "./ic_disc-02.svg"
+import icChromecast from "./ic_chrome-cast.svg"
+import icSliders04 from "./ic_sliders-04.svg"
+
+import imgGradient from "./img_gradient.png"
+import imgSmallSphere from "./img_4-small.png"
+import imgGrid from "./img_grid.png"
+import imgLoading from "./img_loading.png"
+import imgBackground from "./img_background.jpg"
+
+export {
+   icBrainwave,
+   icCheck,
+   icBrainwaveSymbol,
+   icBrainwaveWhiteSymbol,
+   icPlay,
+   icCheck2,
+   icLoading1,
+   icYourlogo,
+   icHomeSmile,
+   icFile02,
+   icSearchMd,
+   icPlusSquare,
+   icRecording03,
+   icRecording01,
+   icDisc02,
+   icChromecast,
+   icSliders04,
+   imgGradient,
+   imgSmallSphere,
+   imgGrid,
+   imgLoading,
+   imgBackground,
+}
+
+// Hero Assets
+import imgCurve from "./hero/img_curve.png"
+import imgRobot from "./hero/img_robot.jpg"
+import imgHeroBackground from "./hero/img_hero-background.jpg"
+
+export { imgCurve, imgRobot, imgHeroBackground }
+
+// Collaboration Assets
+import icCurve1 from "./collaboration/ic_curve-1.svg"
+import icCurve2 from "./collaboration/ic_curve-2.svg"
+import imgDiscord from "./collaboration/img_discord.png"
+import imgFigma from "./collaboration/img_figma.png"
+import imgFramer from "./collaboration/img_framer.png"
+import imgNotion from "./collaboration/img_notion.png"
+import imgPhotoshop from "./collaboration/img_photoshop.png"
+import imgProtopie from "./collaboration/img_protopie.png"
+import imgRaindrop from "./collaboration/img_raindrop.png"
+import imgSlack from "./collaboration/img_slack.png"
+
+export {
+   icCurve1,
+   icCurve2,
+   imgDiscord,
+   imgFigma,
+   imgFramer,
+   imgNotion,
+   imgPhotoshop,
+   imgProtopie,
+   imgRaindrop,
+   imgSlack,
+}
+
+// Services Assets
+import imgService1 from "./services/img_service-1.png"
+import imgService2 from "./services/img_service-2.png"
+import imgService3 from "./services/img_service-3.png"
+
+export { imgService1, imgService2, imgService3 }
+
+// Pricing Assets
+import icLines from "./pricing/ic_lines.svg"
+import icStars from "./pricing/ic_stars.svg"
+
+export { icLines, icStars }
+
+// Roadmap Assets
+import icDone from "./roadmap/ic_done.svg"
+import icUndone from "./roadmap/ic_undone.svg"
+import imgCoins from "./roadmap/img_coins.png"
+import imgHero from "./roadmap/img_hero.png"
+import imgRoadmap1 from "./roadmap/img_image-1.png"
+import imgRoadmap2 from "./roadmap/img_image-2.png"
+import imgRoadmap3 from "./roadmap/img_image-3.png"
+import imgRoadmap4 from "./roadmap/img_image-4.png"
+
+export { icDone, icUndone, imgCoins, imgHero, imgRoadmap1, imgRoadmap2, imgRoadmap3, imgRoadmap4 }
+
+// Notification Assets
+import imgNotification1 from "./notification/img_image-1.png"
+import imgNotification2 from "./notification/img_image-2.png"
+import imgNotification3 from "./notification/img_image-3.png"
+import imgNotification4 from "./notification/img_image-4.png"
+
+export { imgNotification1, imgNotification2, imgNotification3, imgNotification4 }
+
+// Benefits Assets
+import icBenefitCard1 from "./benefits/ic_card-1.svg"
+import icBenefitCard2 from "./benefits/ic_card-2.svg"
+import icBenefitCard3 from "./benefits/ic_card-3.svg"
+import icBenefitCard4 from "./benefits/ic_card-4.svg"
+import icBenefitCard5 from "./benefits/ic_card-5.svg"
+import icBenefitCard6 from "./benefits/ic_card-6.svg"
+import icBenefitIcon1 from "./benefits/ic_icon-1.svg"
+import icBenefitIcon2 from "./benefits/ic_icon-2.svg"
+import icBenefitIcon3 from "./benefits/ic_icon-3.svg"
+import icBenefitIcon4 from "./benefits/ic_icon-4.svg"
+import icBenefitImage2 from "./benefits/img_image-2.png"
+
+export {
+   icBenefitCard1,
+   icBenefitCard2,
+   icBenefitCard3,
+   icBenefitCard4,
+   icBenefitCard5,
+   icBenefitCard6,
+   icBenefitIcon1,
+   icBenefitIcon2,
+   icBenefitIcon3,
+   icBenefitIcon4,
+   icBenefitImage2,
+}
+
+// Socials Assets
+import icDiscordBlack from "./socials/ic_discord.svg"
+import icFacebook from "./socials/ic_facebook.svg"
+import icInstagram from "./socials/ic_instagram.svg"
+import icTelegram from "./socials/ic_telegram.svg"
+import icTwitter from "./socials/ic_twitter.svg"
+
+export { icDiscordBlack, icFacebook, icInstagram, icTelegram, icTwitter }
