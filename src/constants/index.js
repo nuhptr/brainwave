a9e3f511fdcb4acd09052b4e8aa58dd8b98cb76c
nuhptr <nugrahaadi733@gmail.com>
2024-04-26 22:38:31 +0700

    constant: done

--- src/constants/index.js
+++ src/constants/index.js
@@ -52,6 +52,173 @@ export const brainwaveServicesIcons = [
    icSliders04,
 ]
 
+export const collabText =
+   "With smart automation and top-notch security, it's the perfect solution for teams looking to work smarter."
+
+export const collabContent = [
+   {
+      id: "0",
+      title: "Seamless Integration",
+      text: collabText,
+   },
+   {
+      id: "1",
+      title: "Smart Automation",
+   },
+   {
+      id: "2",
+      title: "Top-notch Security",
+   },
+]
+
+export const collabApps = [
+   {
+      id: "0",
+      title: "Figma",
+      icon: imgFigma,
+      width: 26,
+      height: 36,
+   },
+   {
+      id: "1",
+      title: "Notion",
+      icon: imgNotion,
+      width: 34,
+      height: 36,
+   },
+   {
+      id: "2",
+      title: "Discord",
+      icon: imgDiscord,
+      width: 36,
+      height: 28,
+   },
+   {
+      id: "3",
+      title: "Slack",
+      icon: imgSlack,
+      width: 34,
+      height: 35,
+   },
+   {
+      id: "4",
+      title: "Photoshop",
+      icon: imgPhotoshop,
+      width: 34,
+      height: 34,
+   },
+   {
+      id: "5",
+      title: "Protopie",
+      icon: imgProtopie,
+      width: 34,
+      height: 34,
+   },
+   {
+      id: "6",
+      title: "Framer",
+      icon: imgFramer,
+      width: 26,
+      height: 34,
+   },
+   {
+      id: "7",
+      title: "Raindrop",
+      icon: imgRaindrop,
+      width: 38,
+      height: 32,
+   },
+]
+
+export const pricing = [
+   {
+      id: "0",
+      title: "Basic",
+      description: "AI chatbot, personalized recommendations",
+      price: "0",
+      features: [
+         "An AI chatbot that can understand your queries",
+         "Personalized recommendations based on your preferences",
+         "Ability to explore the app and its features without any cost",
+      ],
+   },
+   {
+      id: "1",
+      title: "Premium",
+      description: "Advanced AI chatbot, priority support, analytics dashboard",
+      price: "9.99",
+      features: [
+         "An advanced AI chatbot that can understand complex queries",
+         "An analytics dashboard to track your conversations",
+         "Priority support to solve issues quickly",
+      ],
+   },
+   {
+      id: "2",
+      title: "Enterprise",
+      description: "Custom AI chatbot, advanced analytics, dedicated account",
+      price: null,
+      features: [
+         "An AI chatbot that can understand your queries",
+         "Personalized recommendations based on your preferences",
+         "Ability to explore the app and its features without any cost",
+      ],
+   },
+]
+
+export const benefits = [
+   {
+      id: "0",
+      title: "Ask anything",
+      text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+      backgroundUrl: "./src/assets/benefits/card-1.svg",
+      iconUrl: icBenefitIcon1,
+      imageUrl: icBenefitImage2,
+   },
+   {
+      id: "1",
+      title: "Improve everyday",
+      text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+      backgroundUrl: "./src/assets/benefits/card-2.svg",
+      iconUrl: icBenefitIcon2,
+      imageUrl: icBenefitImage2,
+      light: true,
+   },
+   {
+      id: "2",
+      title: "Connect everywhere",
+      text: "Connect with the AI chatbot from anywhere, on any device, making it more accessible and convenient.",
+      backgroundUrl: "./src/assets/benefits/card-3.svg",
+      iconUrl: icBenefitIcon3,
+      imageUrl: icBenefitImage2,
+   },
+   {
+      id: "3",
+      title: "Fast responding",
+      text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+      backgroundUrl: "./src/assets/benefits/card-4.svg",
+      iconUrl: icBenefitIcon4,
+      imageUrl: icBenefitImage2,
+      light: true,
+   },
+   {
+      id: "4",
+      title: "Ask anything",
+      text: "Lets users quickly find answers to their questions without having to search through multiple sources.",
+      backgroundUrl: "./src/assets/benefits/card-5.svg",
+      iconUrl: icBenefitIcon1,
+      imageUrl: icBenefitImage2,
+   },
+   {
+      id: "5",
+      title: "Improve everyday",
+      text: "The app uses natural language processing to understand user queries and provide accurate and relevant responses.",
+      backgroundUrl: "./src/assets/benefits/card-6.svg",
+      iconUrl: icBenefitIcon2,
+      imageUrl: icBenefitImage2,
+   },
+]
+
 export const navigation = [
    {
       id: "0",
@@ -84,6 +251,39 @@ export const navigation = [
       title: "Sign In",
       url: "#login",
       onlyMobile: true,
+   },
+]
+
+export const socials = [
+   {
+      id: "0",
+      title: "Discord",
+      iconUrl: icDiscordBlack,
+      url: "#",
+   },
+   {
+      id: "1",
+      title: "Twitter",
+      iconUrl: icTwitter,
+      url: "#",
+   },
+   {
+      id: "2",
+      title: "Instagram",
+      iconUrl: icInstagram,
+      url: "#",
+   },
+   {
+      id: "3",
+      title: "Telegram",
+      iconUrl: icTelegram,
+      url: "#",
+   },
+   {
+      id: "4",
+      title: "Facebook",
+      iconUrl: icFacebook,
+      url: "#",
    },
 ]
 
