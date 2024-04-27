89ad54177387b8659a4d1873b866815cce768dd2
nuhptr <nugrahaadi733@gmail.com>
2024-04-27 12:58:42 +0700

    components: ornamen every section update code

---
+++ src/components/design/collaboration-ornamen.jsx
@@ -0,0 +1,17 @@
+import { icCurve1, icCurve2 } from "../../assets"
+
+export function RightCurve() {
+   return (
+      <div className="hidden absolute top-1/2 left-full w-[10.125rem] -mt-1 ml-10 pointer-events-none xl:block">
+         <img src={icCurve2} width={162} height={76} alt="Curve 2" />
+      </div>
+   )
+}
+
+export function LeftCurve() {
+   return (
+      <div className="hidden absolute top-1/2 right-full w-[32.625rem] -mt-1 mr-10 pointer-events-none xl:block">
+         <img src={icCurve1} width={522} height={182} alt="Curve 1" />
+      </div>
+   )
+}
