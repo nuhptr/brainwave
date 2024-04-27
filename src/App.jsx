e631fdef9e1807303477db3afd6cbb6512b2c9ff
nuhptr <nugrahaadi733@gmail.com>
2024-04-27 12:57:14 +0700

    src: app update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import Header from "./components/section/header"
+import Hero from "./components/section/hero"
+
+import ButtonGradient from "./assets/svg/button-gradient"
+
 export default function App() {
-   return <div>App</div>
+   return (
+      <>
+         <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+            <Header />
+            <Hero />
+         </div>
+
+         <ButtonGradient />
+      </>
+   )
 }
