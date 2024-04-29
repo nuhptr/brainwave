197680f3c06d1c9ad6b34b60ab6f95b199ca6be6
nuhptr <nugrahaadi733@gmail.com>
2024-04-29 10:53:44 +0700

    src: app update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import Header from "./sections/header"
 import Hero from "./sections/hero"
+import Benefits from "./sections/benefits"
+import Collaboration from "./sections/collaboration"
+import Services from "./sections/services"
+import Roadmap from "./sections/roadmap"
+import Footer from "./sections/footer"
+import Pricing from "./sections/pricing"
 
 import ButtonGradient from "./assets/svg/button-gradient"
 
@@ -9,6 +15,12 @@ export default function App() {
          <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
             <Header />
             <Hero />
+            <Benefits />
+            <Collaboration />
+            <Services />
+            <Pricing />
+            <Roadmap />
+            <Footer />
          </div>
 
          <ButtonGradient />
